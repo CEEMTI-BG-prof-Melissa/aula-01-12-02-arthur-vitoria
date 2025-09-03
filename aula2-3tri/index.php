4109f15a8b3de4370c8249afab0111d42bc6454b
arthurcastilho239 <arthur.dcbandeira@aluno.edu.es.gov.br>
2025-09-03 11:58:03 -0300

    Create index.php

---
+++ aula2-3tri/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Aula 02</title>
+    </head> 
+        <body>
+            <?php
+            $nome1 = "Arthur";
+            $nome2 = "Vitória";
+            
+            echo "Meu Nome é ".$nome2 ." e minha dupla é " .$nome1; 
+            ?>
+
+        </body> 
+</html>
+        
