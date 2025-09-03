9cbd63ea4a391d540989218418603f55fcfa8238
arthurcastilho239 <arthur.dcbandeira@aluno.edu.es.gov.br>
2025-09-03 11:56:57 -0300

    Create index.php

---
+++ aula1-3tri/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 01 PHP></title>
+    </head>
+    <body>
+        <?php
+           echo "olá, mundo";
+           echo "Arthur e Vitória Castilho";
+        ?>
+    </body>
+<html>
