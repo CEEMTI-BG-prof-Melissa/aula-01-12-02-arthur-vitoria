4eccf8fbf7b1e049c3a68223c8f5b09587926be1
arthurcastilho239 <arthur.dcbandeira@aluno.edu.es.gov.br>
2025-09-09 11:58:43 -0300

    Create index.php

---
+++ aula03-3ºtrimestre/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Aula 03 - PHP</title>
+    </head> 
+        <body>
+            <?php
+            $nome1 = "Arthur";
+            $nome2 = "Vitória";
+            
+            $num1=10;
+            $num2=2;
+
+            $soma=$num1+$num2;
+
+            echo "Meu Nome é ".$nome2 ." e minha dupla é " .$nome1; 
+            echo "<br>Meu Nome é $nome2 e minha dupla é $nome1";
+
+            echo "<br><br>A soma de $num1 + $num2 é igual a $soma. ";
+
+            ?>
+        </body> 
+</html>
+        
